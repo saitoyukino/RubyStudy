ffa7a85c6e8773ac70252d5d40809cf0f2241d5b
inoue <inomasa224@gmail.com>
2017-06-25 12:18:33 +0900

    Q6 add file

--- submit/inoue/Q6/test2.rb
+++ submit/inoue/Q6/test2.rb
@@ -44,15 +44,18 @@ def discount
     g[count] = gets.chomp
     # p g
 
-    g[count] = g[count].to_i(10)
-
     if g[count] == 'end'
-      # p g.inject(:+)
-      break
-    elsif g[count] == 0
-      # p g.inject(:+)
+      puts 'End'
+      g[count] = g[count].to_i(10)
       break
     end
+
+    g[count] = g[count].to_i(10)
+
+    next unless g[count] == 0
+    # p g.inject(:+)
+    puts 'Error'
+    exit
   end
 
   result = g.inject(:+) * 0.8
