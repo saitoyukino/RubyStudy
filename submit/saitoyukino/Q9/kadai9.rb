a1b3d23d2f5e52eca1a7c5dc9da6b629536fa578
saitoyukino <15d2030003sy@edu.tech.ac.jp>
2017-07-22 16:34:55 +0900

    0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722 0722

---
+++ submit/saitoyukino/Q9/kadai9.rb
@@ -0,0 +1,22 @@
+def to_eng_month(month)
+  hash_month = {1 => "January",2 => "February",3 => "March",4 => "April",5 => "May",6 => "June",7 => "July",8 => "August",9 => "September",10 => "October",11 => "November",12 => "December"}
+  hash_month[month]
+end
+
+p to_eng_month(5) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
