de4e56d385bb8ecd6543083dde0ed8899fbf82e4
Takanashi <rameshgiri@my.com>
2017-10-14 14:04:38 +0900

    Q12 commit

--- submit/takanashi/Q12/Q12_05.rb
+++
@@ -1,9 +0,0 @@
-count = 1
-mtp = 2
-
-while count < 5 do
-    origin = 1
-    print ((origin *= mtp) * 5)
-    print ("\n")
-    count += 1
-end
---
+++ submit/takanashi/Q12/Q12_3_1.rb
@@ -0,0 +1,12 @@
+for i in 2..5
+    origin = 1
+    for j in 1..5
+        print(" ")
+        if j == 1
+            print(origin)
+            next
+        end
+        print(origin *= i)
+    end
+    print("\n")
+end
