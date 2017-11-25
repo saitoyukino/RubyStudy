af7721445b27be2a942d64da35480a51ef8f8813
GIRI RAMESH <rameshgiri2016@gmail.com>
2017-11-24 20:46:32 -0800

    Update question18.rb

--- submit/Ramesh/Q18/question18.rb
+++ submit/Ramesh/Q18/question18.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'openssl'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 page = open("https://jp.reuters.com/article/column-forexforum-daisuke-karakama-idJPKBN1DO072", &:read)
-title = page.scan(/<h1.*?>(.*)<.*>/)
+title = page.scan(/<h1.*?>(.*)<\/h1>/)
 p title
 text = page.scan(/<div class="ArticleBody_body_2ECha">(.*)<\/div>/)
 p text
