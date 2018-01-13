790e2e6822b63fb6c45855e04b3549b61a827d78
saitoyukino <15d2030003sy@edu.tech.ac.jp>
2018-01-13 16:07:42 +0900

    Q22

---
+++ submit/saitoyukino/Q22/300113_kadai.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+require 'openssl'
+require 'nokogiri'
+require 'anemone'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+opts = {
+	:storage => Anemone::Storage::PStore('pstore.db'),
+	:depth_limit => 1,
+	:obey_robots_txt => true	
+}
+Anemone.crawl("https://movies.yahoo.co.jp/ranking/index.html",opts) do |doc|
+  doc.on_every_page do |page|
+    
+    title = page.doc.xpath('//*[@id="mv"]/div/div[2]/h1/span').text
+    if(title.empty?)
+      next
+    end
+    p title
+    tmp = page.doc.xpath('//*[@id="mv"]/div/div[2]/p[2]/span/span').text
+    if (tmp.empty?)
+       p page.doc.xpath('//*[@id="mv"]/div/div[2]/p[3]/span/span').text
+    else
+      p tmp
+    end
+    if (tmp.empty?)
+      p page.doc.xpath('//*[@id="mv"]/div/section').text
+    else
+      p tmp
+    end
+  end
+  doc.focus_crawl do |page|
+    page.links.keep_if do |link|
+  	  link.to_s.match(/movies.yahoo.co.jp\/movie/)
+    end
+  end
+end
